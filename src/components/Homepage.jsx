b012d7493e77bab4f219e80679054aa4cf474820
digambarJail <ddjail2004@gmail.com>
2024-05-06 16:59:44 +0530

    minor changes

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -5,12 +5,16 @@ import Google from '/Google_2015_logo.svg'; // Update the path to your Google lo
 const Homepage = () => {
     return (
         <div className="flex flex-col items-center mt-[10%] h-screen bg-gray-100">
+            {/* Render the Google logo */}
             <img src={Google} alt="Google Logo" className="mb-8 h-[15%]" />
             <div className="w-full max-w-md">
+                {/* Render the Search component */}
                 <Search />
             </div>
             <div className="mt-4 space-x-4">
+                {/* Render the Google Search button */}
                 <button className='bg-slate-100 text-gray py-2 px-4 rounded-md font-light focus:border-gray-200 focus:outline-none focus:ring focus:ring-gray-400'>Google Search</button>
+                {/* Render the "I'm Feeling Lucky" button */}
                 <button className='bg-slate-100 text-gray py-2 px-4 rounded-md font-light focus:border-gray-200 focus:outline-none focus:ring focus:ring-gray-400'>I'm Feeling Lucky</button>
             </div>
         </div>
